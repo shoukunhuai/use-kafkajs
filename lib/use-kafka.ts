0ca3cfed5d59d8e645dab587a6302754dbc01225
Shoukun Huai <shoukunhuai@gmail.com>
2022-03-16 23:33:57 +0800

    WIP

--- lib/use-kafka.ts
+++ lib/use-kafka.ts
@@ -1,10 +1,21 @@
-import {Router, IRouter} from "express";
+import {
+    Router,
+    IRouter,
+    Request as Req,
+    Response as Resp,
+    NextFunction as NextFn,
+} from "express";
 
-import {Cluster as ICluster} from "kafkajs";
+
+import {
+    Cluster as ICluster,
+    TopicOffsets,
+    PartitionMetadata, ISocketFactory,
+} from "kafkajs";
 
 const {
     createLogger,
-    LEVELS: {INFO, DEBUG},
+    LEVELS: {INFO},
 } = require("kafkajs/src/loggers");
 
 const LoggerConsole = require("kafkajs/src/loggers/console");
@@ -13,7 +24,21 @@ const defaultSF = require("kafkajs/src/network/socketFactory");
 
 export type HTTP_METHOD = "GET" | "POST" | "DELETE" | "PUT";
 
-export type MESSAGE_ENCODING = "json";
+export type MESSAGE_ENCODING = "json" | "raw";
+
+const MEGA: number = 1024 * 1024;
+
+const json = (h: (req: any, res: any, next: NextFn) => Promise<any>) => {
+    return (req: Req, res: Resp, next: NextFn): Promise<any> => {
+        return h(req, res, next).then(x => res.json(x))
+            .catch((e: Error) => {
+                console.error(e);
+                res.status(500).json({
+                    errorMsg: e.message,
+                });
+            });
+    };
+};
 
 export interface PushConfig {
 
@@ -36,29 +61,67 @@ export interface PushConfig {
     batchSize?: number;
 }
 
-
 export interface PullConfig {
     alias?: string;
-
     topic: string;
 
-    groupId?: string;
+    minBytes?: number;
+    maxBytes?: number;
+    /**
+     * Max wait time in milliseconds.
+     */
+    maxWait?: number;
+    maxBytesPerPartition?: number;
 
+    deserializer?: (buf: Buffer) => any;
+}
+
+interface FetchResponseMessage {
+    timestamp?: string;
+    offset: string;
+    key?: string | Buffer;
+    value: Buffer;
+    // headers?
+}
+
+interface FetchResponse {
+    topicName: string;
+    partitions: {
+        partition: number;
+        messages: FetchResponseMessage[];
+    }[];
 }
 
 export class Kfk {
 
     cluster: ICluster;
 
-    pollTimeout?: number;
+    maxBytes: number;
 
-    maxBytes?: number;
+    maxBytesPerPartition: number;
 
-    constructor(brokers: string[]) {
+    maxWait: number;
+
+    pullConfigs: PullConfig[];
+
+    constructor({
+                    brokers,
+                    maxBytes = MEGA * 10,
+                    maxBytesPerPartition = MEGA,
+                    maxWait = 5000,
+                    pullConfigs = [],
+                }: KafkaOptions) {
+
+
+        this.maxWait = maxWait;
+        this.maxBytes = maxBytes;
+        this.maxBytesPerPartition = maxBytesPerPartition;
+        this.pullConfigs = pullConfigs;
+
         this.cluster = new KafkaCluster({
             brokers,
             logger: createLogger({
-                level: DEBUG,
+                level: INFO,
                 logCreator: LoggerConsole,
             }),
             socketFactory: defaultSF(),
@@ -67,103 +130,206 @@ export class Kfk {
     }
 
     async connect(): Promise<void> {
-        await this.cluster.connect();
+        const {cluster, pullConfigs} = this;
+        await cluster.connect();
 
-        await this.cluster.addTargetTopic("test_logs");
+        if (pullConfigs.length > 0) {
+            await Promise.all(
+                pullConfigs.map(({topic}) => cluster.addTargetTopic(topic)));
+        }
     }
 
-    async disconnect(): Promise<void> {
-        await this.cluster.disconnect();
+    disconnect(): Promise<void> {
+        return this.cluster.disconnect();
     }
 
-    async fetch(topic: string, partition: number = 0, offset: string = "0", maxBytes: number = 1024 * 1024): Promise<any> {
-        const md = this.cluster.findTopicPartitionMetadata(topic).filter(p => p.partitionId === partition);
+    getPartitionMetadata(topic: string): PartitionMetadata[] {
+        const partitions = this.cluster.findTopicPartitionMetadata(topic);
 
-        const b = await this.cluster.findBroker({
-            nodeId: md[0].leader.toString(),
-        });
+        if (partitions.length === 0) {
+            throw new Error("Topic not found, please try again later");
+        }
 
-        return b.fetch({
+        return partitions;
+    }
+
+    /**
+     * Fetch offsets of a topic
+     * @param topic The topic
+     * @param from From where to fetch the offset, beginning/last, or a specific time.
+     */
+    async offsetFetch(topic: string, from: boolean | number = false):
+        Promise<TopicOffsets | undefined> {
+
+        const partitions = this.getPartitionMetadata(topic)
+            .map(p => ({partition: p.partitionId}));
+
+        if (typeof from === "boolean") {
+            return this.cluster.fetchTopicsOffset([{
+                topic,
+                partitions,
+                fromBeginning: from,
+            }]).then(x => x.shift());
+        } else {
+            return this.cluster.fetchTopicsOffset([{
+                topic,
+                partitions,
+                fromTimestamp: from,
+            }]).then(x => x.shift());
+        }
+    }
+
+    async fetch(config: PullConfig,
+                partitionId: number,
+                offset?: string): Promise<any> {
+
+        const {
+            topic,
+            maxBytes = this.maxBytes,
+            maxBytesPerPartition = this.maxBytesPerPartition,
+            maxWait = this.maxWait,
+            deserializer: des,
+        } = config;
+
+        const nodeId = this.cluster.findTopicPartitionMetadata(topic)
+            .filter(p => p.partitionId === partitionId)
+            .map(p => p.leader.toString())
+            .shift();
+
+        if (!nodeId) {
+            throw new Error("Topic not found, please try again later");
+        }
+
+        if (!offset) {
+
+            const offsets = await this.offsetFetch(topic, false);
+            if (!offsets) {
+                throw new Error("Topic not found");
+            }
+
+            offset = offsets.partitions.filter(p => p.partition === partitionId)
+                .map(p => p.offset)
+                .shift();
+            if (!offset) {
+                throw new Error("Partition not found: " + partitionId);
+            }
+        }
+        const fetchOffset = offset;
+
+        return this.cluster.findBroker({nodeId}).then(b => b.fetch({
+            maxWaitTime: maxWait,
+            maxBytes,
             topics: [{
                 topic,
                 partitions: [{
-                    partition,
-                    fetchOffset: offset,
-                    maxBytes: 1024 * 1024,
+                    partition: partitionId,
+                    fetchOffset,
+                    maxBytes: maxBytesPerPartition,
                 }],
             }],
-        });
+        }).then(resp => {
+            const {responses} = resp as {
+                responses: FetchResponse[];
+            };
 
+            let messages = responses.shift()?.partitions?.shift()?.messages;
+            let size = messages?.length || 0;
+            if (messages && size > 0) {
+                const seq = messages[size - 1].offset;
+                if (seq === fetchOffset) {
+                    size = 0;
+                }
+
+                return {
+                    size,
+                    ack: fetchOffset,
+                    seq,
+                    messages: size === 0 ? [] :
+                        messages.map(({key, value, timestamp, offset}) => ({
+                            offset,
+                            timestamp,
+                            key,
+                            value: des ? des(value) : value.toString(),
+                        })),
+                };
+            } else {
+                return {
+                    size: 0,
+                    ack: fetchOffset,
+                    seq: fetchOffset,
+                    messages: [],
+                };
+            }
+        }));
     }
-
-    async subscribe(topic: string, handler: () => Promise<any>) {
-        // subscribe to topic
-    }
-
 }
 
 export interface KafkaOptions {
     brokers: string[];
 
-    alias?: string;
+    maxWait?: number;
+
+    maxBytes?: number;
+
+    maxBytesPerPartition?: number;
 
     pushConfigs?: Array<PushConfig>;
 
     pullConfigs?: Array<PullConfig>;
+
+    socketFactory?: ISocketFactory;
 }
 
 export interface MountOptions {
     expressApp?: IRouter;
+    mountPoint?: string;
 }
 
-export function useKafka({
-                             brokers,
-                             alias,
-                             pullConfigs = [],
-                             pushConfigs = [],
-                         }: KafkaOptions, options: MountOptions = {
-    expressApp: undefined,
-}): Kfk {
+interface Query {
+    ack?: string;
+    seq?: string;
+    timeout?: string;
+    ts?: string;
+    partition?: string;
+}
 
-    console.log("brokers= %s", brokers);
+export function useKafka(
+    options: KafkaOptions,
+    mountOptions: MountOptions = {}): Kfk {
+    const kfk = new Kfk(options);
 
-    const kfk = new Kfk(brokers);
+    const {pullConfigs} = options;
 
-    const {expressApp} = options;
+    const {expressApp, mountPoint = "/kfk/"} = mountOptions;
 
     if (expressApp) {
         const rt = Router();
+        pullConfigs?.forEach((config) => {
+            const {topic, alias} = config;
+            const path = alias || topic;
+            rt.get(`/topics/${path}/records`, json(req => {
+                const {seq} = req.query as Query;
+                let {partition} = req.query;
+                partition = partition ? parseInt(partition) : 0;
 
-        rt.get("/topics/:topic", (req, res) => {
-            const {topic} = req.params;
+                if (isNaN(partition)) {
+                    return Promise.reject("Invalid partition: " + partition);
+                }
 
-            console.log("topic=%s", topic);
+                return kfk.fetch(config, partition, seq);
+            }));
 
-            res.json(kfk.cluster.findTopicPartitionMetadata(topic));
+            rt.get(`/topics/${path}/offsets`, json((req) => {
+                let {ts} = req.query;
+                ts = ts ? isNaN(ts = parseInt(ts)) ? false :
+                    ts === 0 ? true : ts : false;
+
+                return kfk.offsetFetch(topic, ts);
+            }));
         });
 
-        rt.get("/consumers/:group/topics/:topic/records", (req, res) => {
-            const {topic} = req.params;
-            const {offset} = req.query;
-
-            kfk.fetch(topic, 0, typeof offset === "string" ? offset : "0").then(v => res.json(v));
-
-        });
-
-        pullConfigs.forEach(({alias, topic, groupId}) => {
-            rt.get(`/consumers/${groupId}/${topic}/offsets`, (req, res) => {
-                res.json(kfk.cluster.findTopicPartitionMetadata(topic));
-            });
-        });
-
-        // GET /consumers/:group/:topic/offsets
-        // GET /consumers/:group/:topic/:partition/records
-
-        // rt.get("/consumers/:groupId/topics/:topic/offsets");
-
-        expressApp.use("/kfk/", rt);
+        expressApp.use(mountPoint, rt);
     }
-
 
     return kfk;
 }
--- server.ts
+++ server.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 const express = require("express");
 const cors = require("cors");
 
+import {Request} from "express";
+
 
 import {useKafka} from "./lib/use-kafka";
 
@@ -11,13 +13,28 @@ app.use(cors());
 app.use(express.raw({limit: "1mb"}));
 app.use(express.json({limit: "1mb"}));
 
+// apply security check
+app.use("/kfk/", async (req: Request, res: any, next: () => any) => {
+    console.warn("%s %s", req.method, req.url);
+    next();
+});
 
 const kfk = useKafka({
-    brokers: [process.env.KAFKA_BROKERS || 'localhost:9092'],
+    brokers: [process.env.KAFKA_BROKERS || "localhost:9092"],
+    pullConfigs: [{
+        alias: "logs",
+        topic: "test_logs",
+        deserializer: buf => JSON.parse(buf.toString()),
+        maxBytes: 102400,
+        maxBytesPerPartition: 10240,
+        maxWait: 2000,
+    }]
 }, {
-    expressApp: app
+    expressApp: app,
 });
 
 kfk.connect().then(() => {
-    app.listen(9000);
+    app.listen(9000, ()=> {
+        console.log("Have fun with Kafka!");
+    });
 });
